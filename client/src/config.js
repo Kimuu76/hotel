2fbcf640fbcdbf797832de3ae6c992f76c97e1dd
Kimuu76 <officialkimutai@gmail.com>
2025-04-02 21:42:11 +0300

    made changes

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 /** @format */
 /** @format */
 
-//const API_BASE_URL = "https://kevtech-joint-backend.onrender.com";
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://kevtech-joint-backend.onrender.com";
+//const API_BASE_URL = "http://localhost:5000";
 export default API_BASE_URL;
